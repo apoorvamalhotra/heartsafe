fc03abaaa929f58b9dbc6b7e8cd4fbe9e283024e
Apoorva Malhotra <apoorvamalhotra@Apoorvas-MacBook-Air.local>
2022-12-05 03:51:51 -0800

    Initial Commit

--- project/app.py
+++ project/app.py
@@ -18,33 +18,33 @@ def webhook():
         input_data={}
         for i in data:
             if i['field']['id']=='gEMD8GuvPTCN':
-                d['weight'] = i['number']
+                input_data['weight'] = i['number']
             elif i['field']['id']=='dDPp3UxDLnFG':
-                d['Age'] = i['number']
+                input_data['Age'] = i['number']
             elif i['field']['id']=='ZPGfo1iDsBwO':
-                d['height'] = i['number']
+                input_data['height'] = i['number']
             elif i['field']['id']=='z6SHNEOtjhEK':
-                d['Sex'] = i['choice']['label']
+                input_data['Sex'] = i['choice']['label']
             elif i['field']['id']=='WhJg8Q3OFuJ9':
-                d['PhysicalHealth'] = i['number']
+                input_data['PhysicalHealth'] = i['number']
             elif i['field']['id']=='FqwRAvDInTu4':
-                d['MentalHealth'] = i['number']
+                input_data['MentalHealth'] = i['number']
             elif i['field']['id']=='HKGhfqjDlWbU':
-                d['PhysicalActivity'] = i['boolean']
+                input_data['PhysicalActivity'] = i['boolean']
             elif i['field']['id']=='IA2EVmsVRJ1h':
-                d['DifficultyWalking'] = i['boolean']
+                input_data['DifficultyWalking'] = i['boolean']
             elif i['field']['id']=='nQDh7Ibe33LH':
-                d['AverageSleep'] = i['number']
+                input_data['AverageSleep'] = i['number']
             elif i['field']['id']=='LOLMp2JC3qJg':
-                d['SmokingStatus'] = i['boolean']
+                input_data['SmokingStatus'] = i['boolean']
             elif i['field']['id']=='f3YPJzD7Q18B':
-                d['DiabetesHistory'] = i['boolean']
+                input_data['DiabetesHistory'] = i['boolean']
             elif i['field']['id']=='Jiyy7Mom0EXG':
-                d['AsthmaHistory'] = i['boolean']
+                input_data['AsthmaHistory'] = i['boolean']
             elif i['field']['id']=='xW3NYa9odtnk':
-                d['AlcoholConsumption'] = i['number']
+                input_data['AlcoholConsumption'] = i['number']
             elif i['field']['id']=='l9W1Q0ebw3Qu':
-                d['Depression'] = i['boolean']
+                input_data['Depression'] = i['boolean']
         result = lr.parse_data(input_data)
         return result, 200
     else:
